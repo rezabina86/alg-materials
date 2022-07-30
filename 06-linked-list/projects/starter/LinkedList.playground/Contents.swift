b70d2073ddd6acb283e51ef78ec9797e52a46674
Reza Bina <rezabina@Rezas-MacBook-Pro.local>
2022-07-30 19:49:36 +0300

    add methods for adding and removing values from the list

--- 06-linked-list/projects/starter/LinkedList.playground/Contents.swift
+++ 06-linked-list/projects/starter/LinkedList.playground/Contents.swift
@@ -1,3 +1,13 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+var list = LinkedList<Int>()
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(4)
+
+list.append(5)
+
+print(list)
--- 06-linked-list/projects/starter/LinkedList.playground/Sources/LinkedList.swift
+++ 06-linked-list/projects/starter/LinkedList.playground/Sources/LinkedList.swift
@@ -11,6 +11,19 @@ public struct LinkedList<Value> {
     
     public init() {}
     
+    public func node(at index: Int) -> Node<Value>?
+    {
+        var currentIndex = 0
+        var currentNode = head
+        
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode?.next
+            currentIndex += 1
+        }
+        
+        return currentNode
+    }
+    
 }
 
 extension LinkedList: CustomStringConvertible {
@@ -20,4 +33,89 @@ extension LinkedList: CustomStringConvertible {
         }
         return String(describing: head)
     }
+}
+
+// MARK: - Adding values to the list
+extension LinkedList {
+    
+    public mutating func push(_ value: Value)
+    {
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    public mutating func append(_ value: Value)
+    {
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        
+        tail!.next = Node(value: value)
+        
+        tail = tail!.next
+    }
+    
+    @discardableResult
+    public mutating func insert(_ value: Value,
+                                after node: Node<Value>) -> Node<Value>
+    {
+        guard node !== tail else {
+            append(value)
+            return tail!
+        }
+        node.next = Node(value: value, next: node.next)
+        return node.next!
+    }
+    
+}
+
+// MARK: - Removing values from the list
+extension LinkedList {
+    
+    @discardableResult
+    public mutating func pop() -> Value?
+    {
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    @discardableResult
+    public mutating func removeLast() -> Value?
+    {
+        guard let head = head else { return nil }
+        guard head.next != nil else { return pop() }
+        
+        var prev = head
+        var current = head
+        
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        
+        prev.next = nil
+        tail = prev
+        return current.value
+    }
+    
+    @discardableResult
+    public mutating func remove(after node: Node<Value>) -> Value?
+    {
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
+    
 }
