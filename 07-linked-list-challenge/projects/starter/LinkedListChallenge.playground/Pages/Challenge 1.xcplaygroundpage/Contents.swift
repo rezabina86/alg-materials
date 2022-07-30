224a4b38ba4d00043893f0f02cdcf957054d4bfb
Reza Bina <tavakolibina@gmail.com>
2022-07-30 20:02:18 +0300

    solves the first two questions of the linkedList section

--- 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
+++ 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
@@ -3,11 +3,16 @@
 /*:
  # Linked List Challenges
  ## Challenge 1: Print in reverse
-
+ 
  Create a function that prints the nodes of a linked list in reverse order.
  */
 func printInReverse<T>(_ list: LinkedList<T>) {
-  
+    printNodeInReverse(list.head)
 }
 
+func printNodeInReverse<T>(_ node: Node<T>?) {
+    guard let node = node else { return }
+    printNodeInReverse(node.next)
+    print(node.value)
+}
 //: [Next Challenge](@next)
