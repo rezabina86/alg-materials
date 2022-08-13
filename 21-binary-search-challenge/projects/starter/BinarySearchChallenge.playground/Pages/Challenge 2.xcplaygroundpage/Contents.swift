cc7a50d04fd6d5a1ed509c582437986e3329fda0
Reza Bina <tavakolibina@gmail.com>
2022-08-13 22:57:41 +0300

    BST challenges

--- 21-binary-search-challenge/projects/starter/BinarySearchChallenge.playground/Pages/Challenge 2.xcplaygroundpage/Contents.swift
+++ 21-binary-search-challenge/projects/starter/BinarySearchChallenge.playground/Pages/Challenge 2.xcplaygroundpage/Contents.swift
@@ -16,10 +16,72 @@ findIndices(of: 3, in: array)
 func findIndices<Elements: RandomAccessCollection>(for element: Elements.Element,
                                                    in collection: Elements) -> Range<Elements.Index>? where Elements.Element: Comparable
 {
-    return nil
+    guard let firstIndice = firstIndice(for: element, in: collection), let lastIndice = lastIndice(for: element, in: collection) else { return nil }
+    return firstIndice..<lastIndice
 }
 
+func firstIndice<Elements: RandomAccessCollection>(for element: Elements.Element,
+                                                   in collection: Elements,
+                                                   in range: Range<Elements.Index>? = nil) -> Elements.Index? where Elements.Element: Comparable
+{
+    let range = range ?? collection.startIndex..<collection.endIndex
+    guard range.lowerBound < range.upperBound else {
+        return nil
+    }
+    
+    let size = collection.distance(from: range.lowerBound, to: range.upperBound)
+    let middleIndex = collection.index(range.lowerBound, offsetBy: size / 2)
+    
+    if middleIndex == collection.startIndex || middleIndex == collection.index(before: collection.endIndex) {
+        if collection[middleIndex] == element {
+            return middleIndex
+        }
+        return nil
+    }
+    
+    if collection[middleIndex] == element {
+        let prevIndex = collection.index(before: middleIndex)
+        if collection[prevIndex] != element {
+            return middleIndex
+        } else {
+            return firstIndice(for: element, in: collection, in: range.lowerBound..<middleIndex)
+        }
+    } else if collection[middleIndex] > element {
+        return firstIndice(for: element, in: collection, in: range.lowerBound..<middleIndex)
+    } else {
+        return firstIndice(for: element,in: collection,in: collection.index(after: middleIndex)..<range.upperBound)
+    }
+}
 
-let array = [1, 2, 3, 3, 3, 4, 5, 5]
-let indices = findIndices(for: 3, in: array)
-print(indices)
+func lastIndice<Elements: RandomAccessCollection>(for element: Elements.Element,
+                                                  in collection: Elements,
+                                                  in range: Range<Elements.Index>? = nil) -> Elements.Index? where Elements.Element: Comparable
+{
+    let range = range ?? collection.startIndex..<collection.endIndex
+    guard range.lowerBound < range.upperBound else {
+        return nil
+    }
+    
+    let size = collection.distance(from: range.lowerBound, to: range.upperBound)
+    let middleIndex = collection.index(range.lowerBound, offsetBy: size / 2)
+    
+    if middleIndex == collection.startIndex || middleIndex == collection.index(before: collection.endIndex) {
+        if collection[middleIndex] == element {
+            return collection.index(after: middleIndex)
+        }
+        return nil
+    }
+    
+    if collection[middleIndex] == element {
+        let nextIndex = collection.index(after: middleIndex)
+        if collection[nextIndex] != element {
+            return nextIndex
+        } else {
+            return firstIndice(for: element,in: collection,in: collection.index(after: middleIndex)..<range.upperBound)
+        }
+    } else if collection[middleIndex] > element {
+        return firstIndice(for: element, in: collection, in: range.lowerBound..<middleIndex)
+    } else {
+        return firstIndice(for: element,in: collection,in: collection.index(after: middleIndex)..<range.upperBound)
+    }
+}
