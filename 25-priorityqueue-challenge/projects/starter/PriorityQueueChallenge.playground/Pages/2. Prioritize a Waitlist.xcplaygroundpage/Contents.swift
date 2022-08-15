3c6d0d554c273a9a3789f3ef0819959dacd3d997
Reza Bina <tavakolibina@gmail.com>
2022-08-15 16:06:34 +0300

    PriorityQueue

--- 25-priorityqueue-challenge/projects/starter/PriorityQueueChallenge.playground/Pages/2. Prioritize a Waitlist.xcplaygroundpage/Contents.swift
+++ 25-priorityqueue-challenge/projects/starter/PriorityQueueChallenge.playground/Pages/2. Prioritize a Waitlist.xcplaygroundpage/Contents.swift
@@ -12,3 +12,22 @@ public struct Person: Equatable {
   let age: Int
   let isMilitary: Bool
 }
+
+let p1 = Person(name: "Josh", age: 21, isMilitary: true)
+let p2 = Person(name: "Jake", age: 22, isMilitary: true)
+let p3 = Person(name: "Clay", age: 28, isMilitary: false)
+let p4 = Person(name: "Cindy", age: 28, isMilitary: false)
+let p5 = Person(name: "Sabrina", age: 30, isMilitary: false)
+
+let waitlist = [p1, p2, p3, p4, p5]
+
+var queue = PriorityQueue(sort: { p1, p2 in
+    if p1.isMilitary == p2.isMilitary {
+        return p1.age > p2.age
+    }
+    return p1.isMilitary
+}, elements: waitlist)
+
+while !queue.isEmpty {
+    print(queue.dequeue()?.name)
+}
--- 25-priorityqueue-challenge/projects/starter/PriorityQueueChallenge.playground/Sources/PriorityQueue.swift
+++ 25-priorityqueue-challenge/projects/starter/PriorityQueueChallenge.playground/Sources/PriorityQueue.swift
@@ -4,27 +4,31 @@
 import Foundation
 
 public struct PriorityQueue<Element: Equatable>: Queue {
-  
-  private var heap: Heap<Element>
-  
-  public init(sort: @escaping (Element, Element) -> Bool, elements: [Element] = []) {
-    heap = Heap(sort: sort, elements: elements)
-  }
-  
-  public var isEmpty: Bool {
-    heap.isEmpty
-  }
-  
-  public var peek: Element? {
-    heap.peek()
-  }
-  
-  public mutating func enqueue(_ element: Element) -> Bool {
-    heap.insert(element)
-    return true
-  }
-  
-  public mutating func dequeue() -> Element? {
-    heap.remove()
-  }
+    
+    private var heap: Heap<Element>
+    
+    public init(sort: @escaping (Element, Element) -> Bool, elements: [Element] = []) {
+        heap = Heap(sort: sort, elements: elements)
+    }
+    
+    public var isEmpty: Bool {
+        heap.isEmpty
+    }
+    
+    public var peek: Element? {
+        heap.peek()
+    }
+    
+    public var count: Int {
+        heap.count
+    }
+    
+    public mutating func enqueue(_ element: Element) -> Bool {
+        heap.insert(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> Element? {
+        heap.remove()
+    }
 }
